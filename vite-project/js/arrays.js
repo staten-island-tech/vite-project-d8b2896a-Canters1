fe9103fbedfb56ccb42eb3382c094cb1a9fd4944
Yan Sharma <yawning008@gmail.com>
2023-10-30 10:15:38 -0400

    awdasd

--- vite-project/js/arrays.js
+++ vite-project/js/arrays.js
@@ -4,105 +4,120 @@ const movies = [
       year: 1994,
       director: 'Frank Darabont',
       poster: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 80
+      metacritic: 80,
+      Oscar: false,
     },
     {
       title: 'The Godfather',
       year: 1972,
       director: 'Francis Ford Coppola',
       poster: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg',
-      metacritic: 100
+      metacritic: 100,
+      Oscar: true,
     },
     {
       title: 'The Godfather: Part II',
       year: 1974,
       director: 'Francis Ford Coppola', 
       poster: 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg',
-      metacritic: 90
+      metacritic: 90,
+      Oscar: true,
     },
     {
       title: 'The Dark Knight',
       year: 2008,
       director: 'Christopher Nolan',
       poster: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY268_CR0,0,182,268_AL_.jpg',
-      metacritic: 84
+      metacritic: 84,
+      Oscar: false,
     },
     {
       title: '12 Angry Men',
       year: 1957,
       director: 'Sidney Lumet',
       poster: 'https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg', 
-      metacritic: 96
+      metacritic: 96,
+      Oscar: false,
     },
     {
-      title: 'Schindler\'s List',
+      title: "Schindler's List",
       year: 1993,
       director: 'Steven Spielberg',
       poster: 'https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 94
+      metacritic: 94,
+      Oscar: true,
     },
     {
       title: 'The Lord of the Rings: The Return of the King',
       year: 2003,
       director: 'Peter Jackson',
       poster: 'https://m.media-amazon.com/images/M/MV5BYWY1ZWQ5YjMtMDE0MS00NWIzLWE1M2YtODYzYTk2OTNlYWZmXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 94
+      metacritic: 94,
+      Oscar: true,
     },
     {
       title: 'Pulp Fiction',
       year: 1994,
       director: 'Quentin Tarantino',
       poster: 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR0,0,182,268_AL_.jpg',
-      metacritic: 94
+      metacritic: 94,
+      Oscar: false,
     },
     {
       title: 'The Good, the Bad and the Ugly',
       year: 1966,
       director: 'Sergio Leone',
       poster: 'https://m.media-amazon.com/images/M/MV5BOTQ5NDI3MTI4MF5BMl5BanBnXkFtZTgwNDQ4ODE5MDE@._V1_UY268_CR9,0,182,268_AL_.jpg',
-      metacritic: 90
+      metacritic: 90,
+      Oscar: false,
     },
     {
       title: 'Fight Club',
       year: 1999,
       director: 'David Fincher',
       poster: 'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 66
+      metacritic: 66,
+      Oscar: false,
     },
     {
       title: 'Forrest Gump',
       year: 1994,
       director: 'Robert Zemeckis',
-      poster: 'https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR4,0,182,268_AL_.jpg',
-      metacritic: 82
+      poster: 'https://m.media-amaz on.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR4,0,182,268_AL_.jpg',
+      metacritic: 82,
+      Oscar: true,
     },
     {
       title: 'Inception',
       year: 2010,
       director: 'Christopher Nolan',
       poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 74
+      metacritic: 74,
+      Oscar: false,
     },
     {
       title: 'The Lord of the Rings: The Fellowship of the Ring', 
       year: 2001,
       director: 'Peter Jackson',
       poster: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 92
+      metacritic: 92,
+      Oscar: false,
     },
     {
       title: 'Star Wars: Episode V - The Empire Strikes Back',
       year: 1980,
       director: 'Irvin Kershner',
       poster: 'https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 82
+      metacritic: 82,
+      Oscar: false,
     },
     {
       title: 'The Matrix',
       year: 1999,
       director: 'The Wachowskis', 
       poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
-      metacritic: 73
+      metacritic: 73,
+      Oscar: false,
     }
   ];
 

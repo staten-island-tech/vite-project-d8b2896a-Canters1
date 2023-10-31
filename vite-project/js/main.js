b973caeba0d512de400b7ed98892c2fe9c41e046
Yan Sharma <yawning008@gmail.com>
2023-10-31 09:32:20 -0400

    awdasd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,24 @@
 import {movies} from  "./arrays.js" 
-
+const domselectors = {
+    flexwrapper : document.querySelector(".flex-wrapper"),
+}
 console.log(movies)
+function makemovie() {
+
+   return movies.forEach((movie)=>domselectors.flexwrapper.insertAdjacentHTML(
+    "beforeend",
+    `<div class = "card">
+        <img src='${movie.poster}' alt="">
+        <div class="card-content">
+        <h2>
+            ${movie.title}
+        </h2>
+        <p>
+            Directed by: ${movie.director}. Released in ${movie.year}
+        </p>
+
+        </div>
+    </div>`
+  ));
+}
+makemovie()
